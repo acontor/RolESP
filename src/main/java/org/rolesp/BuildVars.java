ed5b6c98155708bc64031d7b7a411be0ff5d4411
sirvaro <alvaroconde03@gmail.com>
2019-06-06 21:34:03 +0200

    v0.1b

--- src/main/java/org/rolesp/BuildVars.java
+++ src/main/java/org/rolesp/BuildVars.java
@@ -4,13 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Ruben Bermudez
- * @version 1.0
- * @brief Custom build vars FILL EVERYTHING CORRECTLY
- * @date 20 of June of 2015
- */
-
 public class BuildVars {
 
     public static final List<Integer> ADMINS = new ArrayList<>();
@@ -25,6 +18,6 @@ public class BuildVars {
 
 
     static {
-        // Add elements to ADMIN array here
+        // Add elements to ADMIN array
     }
 }
